e57b5a1ae77a3ae777c1de994158cd950d08955a
Tim Marlétaz <tim.marletaz@bbzbl-it.ch>
2025-06-04 14:45:56 +0200

    Backend initialization

---
+++ Backend/src/main/java/com/m347/pollit/PollitApplication.java
@@ -0,0 +1,13 @@
+package com.m347.pollit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollitApplication.class, args);
+	}
+
+}
