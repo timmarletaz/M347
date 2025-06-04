b2790b2a7a873311fc4a6238805f257c87ed6db3
Tim Marlétaz <tim.marletaz@bbzbl-it.ch>
2025-06-04 16:10:54 +0200

    First Classes

---
+++ Backend/src/main/java/com/m347/pollit/User.java
@@ -0,0 +1,30 @@
+package com.m347.pollit;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+
+    @OneToMany(mappedBy = "creator")
+    private List<Poll> polls;
+
+}
