5c8a1a002c44974792f2dddb9d63188e5d7308e1
Tim Marlétaz <tim.marletaz@bbzbl-it.ch>
2025-06-04 14:45:56 +0200

    Backend initialization

---
+++ Backend/src/test/java/com/m347/pollit/PollitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m347.pollit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
